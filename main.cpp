c511e11caf0cf6ef7064bf84fecc8ea9201b734b
Jonathan Poelen <jonathan.poelen@gmail.com>
2018-10-09 01:47:17 +0200

    print dependancies

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
-#include <kvasir/mpl/types/list.hpp>
+#include <kvasir/mpl/algorithm/count_if.hpp>
+#include <kvasir/mpl/algorithm/filter.hpp>
+#include <kvasir/mpl/algorithm/flatten.hpp>
+#include <kvasir/mpl/algorithm/fold_left.hpp>
+#include <kvasir/mpl/algorithm/remove_adjacent.hpp>
+#include <kvasir/mpl/functional/bind.hpp>
+#include <kvasir/mpl/functional/call.hpp>
+#include <kvasir/mpl/functional/compose.hpp>
+#include <kvasir/mpl/functional/fork.hpp>
+#include <kvasir/mpl/sequence/front.hpp>
+#include <kvasir/mpl/sequence/join.hpp>
+#include <kvasir/mpl/sequence/pop_front.hpp>
+#include <kvasir/mpl/sequence/push_back.hpp>
 #include <kvasir/mpl/sequence/size.hpp>
+#include <kvasir/mpl/sequence/drop.hpp>
+#include <kvasir/mpl/types/list.hpp>
+#include <kvasir/mpl/types/bool.hpp>
+#include <kvasir/mpl/utility/conditional.hpp>
+#include <kvasir/mpl/utility/same_as.hpp>
+#include <kvasir/mpl/utility/always.hpp>
+
+// // extra kvasir.mpl
+// namespace kvasir::mpl
+// {
+//   // TODO remove_adjacent is probably bogus
+//
+//   template<class P, class C = identity>
+//   struct disjunction
+//   {
+//     template<class... Ts>
+//     using f = typename C::template f<
+//       bool_<typename P::template f<Ts>::value || ...>>;
+//   };
+//
+//   template<class P, class C = identity>
+//   struct conjunction
+//   {
+//     template<class... Ts>
+//     using f = typename C::template f<
+//       bool_<typename P::template f<Ts>::value && ...>>;
+//   };
+// }
 
 #include <utility>
+#include <tuple>
 #include <type_traits>
 #include <string_view>
 
@@ -12,165 +53,257 @@ namespace jln::fmtexpr
   namespace kmp = kvasir::mpl;
   namespace kmpe = kvasir::mpl::eager;
 
-  namespace detail
-  {
-    template<class T, class = void>
-    struct decl_or_t_impl
-    {
-      using type = T;
-    };
-
-    template<class T>
-    struct decl_or_t_impl<T, std::void_t<typename T::decl_type>>
-    {
-      using type = typename T::decl_type;
-    };
-
-    template<class T, class = void>
-    struct get_deps_impl
-    {
-      using type = kmp::list<>;
-    };
-
-    template<class T>
-    struct get_deps_impl<T, std::void_t<typename T::dep_list>>
-    {
-      using type = typename T::dep_list;
-    };
-
-    template<class T, class = void>
-    struct get_name_impl
-    {
-      using type = void;
-    };
-
-    template<class T>
-    struct get_name_impl<T, std::void_t<typename T::name_type>>
-    {
-      using type = typename T::name_type;
-    };
-  }
-
-  template<class T>
-  using get_deps = typename detail::get_deps_impl<typename T::desc_type>::type;
-  template<class T>
-  using get_decl = typename detail::decl_or_t_impl<typename T::desc_type>::type;
-  template<class T>
-  using get_name = typename detail::get_name_impl<typename T::desc_type>::type;
-
-  template<class T>
-  using decl_t = typename T::decl_type;
-
-
-  template<class Desc, class T>
-  struct value
-  {
-    using desc_type = Desc;
-
-    T x;
-  };
-
-  template<class Desc>
-  struct declaration
-  {
-    using desc_type = Desc;
-
-    template<class X>
-    value<Desc, X> operator()(X&& x) const
-    {
-      return {JLN_FWD(X, x)};
-    }
-  };
-
-  template<class Decl>
-  struct primitive
-  {
-    using desc_type = primitive;
-    using decl_type = Decl;
-
-    template<class X>
-    value<primitive, X> operator()(X&& x) const
-    {
-      return {JLN_FWD(X, x)};
-    }
-  };
-
-#define JLN_FMTEXPR_VAR(name, decl)   \
-  constexpr struct name :             \
-    ::jln::fmtexpr::declaration<name> \
-  {                                   \
-    using name_type = name;           \
-    using decl_type = decltype(decl); \
-  } name {}
-
-  template<class Name, class Decl>
-  struct named_decl
-  {
-    using name_type = Name;
-    using decl_type = get_decl<Decl>;
-    using dep_list = get_deps<Decl>;
-  };
-
-  template<class Name, class Decl>
-  declaration<named_decl<Name, Decl>> named(Name /*deps*/, Decl /*decl*/)
-  {
-    return {};
-  }
-
-  template<class Name, class Decl>
-  declaration<named_decl<Name, Decl>> named(Decl /*decl*/)
-  {
-    return {};
-  }
-
   namespace mp
   {
-    template<class T>
-    struct is_value : std::false_type
-    {};
+    using kmp::list;
 
-    template<class Desc, class T>
-    struct is_value<value<Desc, T>> : std::true_type
-    {};
+    template<class F, class... Ts>
+    using eager = typename F::template f<Ts...>;
+
+    template<class... Ls>
+    using join = typename kmp::join<kmp::listify>::template f<Ls...>;
+
+    template<class, class>
+    struct push_back_if_not_found;
+
+    template<class... Ts, class T>
+    struct push_back_if_not_found<list<Ts...>, T>
+    {
+      using type = typename kmp::conditional<(std::is_same<T, Ts>::value || ...)>
+        ::template f<kmp::pop_front<>, kmp::rotate<kmp::int_<1>>>
+        ::template f<T, Ts...>;
+    };
+
+    template<class C = kmp::listify>
+    struct unique_append
+    {
+      template<class L, class... Ts>
+      using f = typename kmp::fold_left<
+        kmp::cfl<push_back_if_not_found>,
+        kmp::unpack<C>
+      >::template f<L, Ts...>;
+    };
+
+    template<class C = kmp::listify>
+    struct unique
+    {
+      template<class... Ts>
+      using f = typename unique_append<C>::template f<list<>, Ts...>;
+    };
   }
+
+  namespace detail
+  {
+    template<template<class> class Tpl, class T, class Default, class = void>
+    struct extract_type
+    {
+      using type = Default;
+    };
+
+    template<template<class> class Tpl, class T, class Default>
+    struct extract_type<Tpl, T, Default, std::void_t<Tpl<T>>>
+    {
+      using type = Tpl<T>;
+    };
+
+    template<template<class> class Tpl, class T, class Default>
+    using extract_type_t = typename extract_type<Tpl, T, Default>::type;
+  }
+
+  template<class T> using binded_variable_t = typename T::binded_variable;
+  template<class T> using desc_type_t = typename T::desc_type;
+  template<class T> using dep_list_t = typename T::dep_list;
+  template<class T> using fmtexpr_type_t = typename T::jln_fmtexpr_type;
+  template<class T> using fmtexpr_expr_t = typename T::jln_fmtexpr_expr;
+
+  template<class T>
+  using get_deps = detail::extract_type_t<dep_list_t, T, kmp::list<>>;
+
+  template<class... Ts>
+  using get_deps_xs = mp::join<get_deps<Ts>...>;
+
+  template<class T>
+  using get_desc = detail::extract_type_t<desc_type_t, T, void>;
+
+  template<class T>
+  using get_binded_variable
+    = detail::extract_type_t<binded_variable_t, T, void>;
+
+  template<class T>
+  using get_fmtexpr_or_type = detail::extract_type_t<fmtexpr_type_t, T, T>;
+
+  template<class T>
+  using get_fmtexpr_or_expr = detail::extract_type_t<fmtexpr_expr_t, T, T>;
+
+  template<class Var, class Desc, class T>
+  struct value
+  {
+    using jln_fmtexpr_type = value;
+    using binded_variable = Var;
+    using desc_type = Desc;
+    using dep_list = get_deps_xs<Desc, T>;
+
+    Desc desc;
+    T x;
+
+    template<class X>
+    constexpr auto& operator()(X&& x) const
+    {
+      return *this;
+    }
+  };
+
+  template<class Var, class Desc>
+  struct statement
+  {
+    using jln_fmtexpr_type = statement;
+    using desc_type = Desc;
+    using dep_list = mp::join<
+      kmpe::conditional<std::is_same_v<Var, void>, kmp::list<>, kmp::list<Var>>,
+      get_deps<Desc>>;
+
+    Desc desc;
+
+    template<class X>
+    constexpr value<Var, Desc, X> operator()(X&& x) const
+    {
+      return {desc, JLN_FWD(X, x)};
+    }
+  };
+
+  class unspecified {};
+
+  template<class Var>
+  using variable = statement<Var, unspecified>;
+
+  template<class Desc>
+  using primitive = statement<void, Desc>;
+
+  template<class Var, class Desc>
+  using typed_variable = statement<Var, Desc>;
+
+  template<class Op, class Lhs, class Rhs>
+  struct expression
+  {
+    using dep_list = kmpe::join<get_deps<Lhs>, get_deps<Rhs>>;
+
+    Op op;
+    Lhs lhs;
+    Rhs rhs;
+
+    template<class X>
+    constexpr auto operator()(X&& x) const
+    {
+      return op(lhs(JLN_FWD(X, x)), rhs(JLN_FWD(X, x)));
+    }
+  };
+
+  template<class Op, class Lhs, class Rhs>
+  expression(Op, Lhs, Rhs) -> expression<Op, Lhs, Rhs>;
+
+
+
+#define JLN_FMT_MAKE_IS(type)                    \
+  template<class T>                              \
+  struct is_##type : std::false_type             \
+  {};                                            \
+                                                 \
+  template<class... Ts>                          \
+  struct is_##type<type<Ts...>> : std::true_type \
+  {};                                            \
+                                                 \
+  template<class T>                              \
+  constexpr inline bool is_##type##_v = is_##type<T>::value
+
+  JLN_FMT_MAKE_IS(value);
+  JLN_FMT_MAKE_IS(statement);
+  JLN_FMT_MAKE_IS(expression);
+
+#undef JLN_FMT_MAKE_IS
+
+  namespace detail
+  {
+    template<class S1>
+    struct statement_minimal
+    {
+      using type = S1;
+    };
+
+    template<class Var, class Desc>
+    struct statement_minimal<statement<Var, statement<void, Desc>>>
+    {
+      using type = statement<Var, Desc>;
+    };
+  }
+
+#define JLN_FMTEXPR_VAR(name)      \
+  constexpr struct name :          \
+    ::jln::fmtexpr::variable<name> \
+  {} name {}
+
+#define JLN_FMTEXPR_TYPED_VAR(name, desc)         \
+  constexpr struct name :                         \
+    ::jln::fmtexpr::detail::statement_minimal<    \
+      ::jln::fmtexpr::typed_variable<             \
+        name, desc_type_t<decltype(desc)>>>::type \
+  {} name {}
+
+  template<class Name>
+  variable<Name> named(Name /*deps*/)
+  {
+    return {};
+  }
+
+  template<class Desc, class Name>
+  typed_variable<Name, Desc> typed(variable<Name> /*var*/, Desc desc)
+  {
+    return {desc};
+  }
+
+  template<class Desc, class Name>
+  typed_variable<Name, Desc> typed(variable<Name> /*var*/)
+  {
+    return {};
+  }
+
+  template<class Name, class Desc>
+  typed_variable<Name, Desc> typed_name(Name /*deps*/, Desc desc)
+  {
+    return {desc};
+  }
+
+  template<class Name, class Desc>
+  typed_variable<Name, Desc> typed_name(Desc desc)
+  {
+    return {desc};
+  }
+
 
   namespace mf
   {
     template<class T>
     constexpr bool check_value(T const&)
     {
-      static_assert(mp::is_value<T>::value, "T isn't jln::fmtexpr::value<...> type");
+      static_assert(is_value_v<T>, "T isn't jln::fmtexpr::value<...> type");
       return true;
     }
   }
 
-  template<class... Xs>
-  struct pack_t : Xs...
+  template<class Pack, class F>
+  auto my_apply(Pack&& pack, F&& f)
   {
-    template<class F>
-    auto apply(F f) &&
-    {
-      return f(static_cast<Xs&&>(*this)...);
-    }
+    return std::apply(JLN_FWD(F, f), JLN_FWD(Pack, pack));
+  }
 
-    template<class F>
-    auto apply(F f) &
-    {
-      return f(static_cast<Xs&>(*this)...);
-    }
-
-    template<class F>
-    auto apply(F f) const&
-    {
-      return f(static_cast<Xs const&>(*this)...);
-    }
-  };
+  using std::apply;
+  template<class... Xs>
+  using pack_t = std::tuple<Xs...>;
 
   template<class... Xs>
   pack_t<std::remove_reference_t<Xs>...> pack(Xs&&... xs)
   {
-    static_assert((mf::check_value(xs) && ...));
-    return {xs...};
+    // static_assert((mf::check_value(xs) && ...));
+    return {JLN_FWD(Xs, xs)...};
   }
 
 
@@ -276,38 +409,90 @@ namespace jln::fmtexpr::ext::print
 
   namespace detail
   {
+#if __clang__
     template<class T>
-    std::string_view print_name()
+    std::string_view get_type_name()
+    {
+      const std::string_view s = __PRETTY_FUNCTION__;
+      const std::size_t start = 72;
+      const std::size_t end = 1;
+      return {s.begin()+start, s.size() - start - end};
+    }
+#else
+    template<class T>
+    std::string_view get_type_name()
     {
       const std::string_view s = __PRETTY_FUNCTION__;
       const std::size_t start = 74;
       const std::size_t end = 50;
       return {s.begin()+start, s.size() - start - end};
     }
+#endif
 
     template<class... Ts>
     void print_deps(kmp::list<Ts...>)
     {
-      ((std::cout << detail::print_name<Ts>() << " "), ...);
+      ((std::cout << detail::get_type_name<Ts>() << "  "), ...);
     }
 
     template<class Value>
     void print_element(Value const& value)
     {
-      std::cout << print_name<Value>() << "\n";
-      std::cout << "  name: " << print_name<get_name<Value>>() << "\n";
+      using fmtexpr_or_type = get_fmtexpr_or_type<Value>;
+      std::cout << get_type_name<Value>() << "\n";
+      if (!std::is_same_v<fmtexpr_or_type, Value>)
+      {
+        std::cout << "  fmtexpr type: " << get_type_name<fmtexpr_or_type>() << "\n";
+      }
+      std::cout << "  linked to: " << get_type_name<get_binded_variable<Value>>() << "\n";
       std::cout << "  dependencies (" << kmpe::size<get_deps<Value>>::value << "): "; print_deps(get_deps<Value>{}); std::cout << "\n";
-      std::cout << "  type: " << print_name<get_decl<Value>>() << "\n";
-      std::cout << "  value_type: " << print_name<decltype(value.x)>() << "\n";
-      std::cout << "  value: " << value.x << "\n";
+      std::cout << "  description: " << get_type_name<get_desc<Value>>() << "\n";
+      if constexpr (is_value_v<fmtexpr_or_type>)
+      {
+        std::cout << "  value_type: " << get_type_name<decltype(value.x)>() << "\n";
+        std::cout << "  value: " << value.x << "\n";
+      }
+      else if constexpr (is_statement_v<fmtexpr_or_type>)
+      {
+        std::cout << "  value: (unevaluated statement)\n";
+      }
+      else if constexpr (is_expression_v<fmtexpr_or_type>)
+      {
+        std::cout << "  value: (unevaluated expression)\n";
+      }
+      else
+      {
+        std::cout << "  value: " << value << "\n";
+      }
+      std::cout << "\n";
     }
   }
 
   template<class Pack>
   void introspection(Pack const& pack)
   {
-    pack.apply([](auto&... xs){
+    my_apply(pack, [](auto&... xs){
       (detail::print_element(xs), ...);
+
+#define T(x) std::remove_reference_t<decltype(x)>
+      using deps = mp::eager<
+        kmp::join<mp::unique<>>,
+        get_deps<T(xs)>...>;
+      using satisfactory_deps = mp::eager<
+        kmp::remove_if<kmp::same_as<void>, mp::unique<>>,
+        get_binded_variable<T(xs)>...>;
+      using unsatisfactory_deps = mp::eager<
+        kmp::unpack<mp::unique_append<kmp::drop<kmpe::size<satisfactory_deps>>>>, deps, satisfactory_deps>;
+#undef T
+
+      std::cout << "dependencies (" << kmpe::size<deps>::value << "): ";
+      detail::print_deps(deps{}); std::cout << "\n";
+
+      std::cout << "satisfactory dependencies (" << kmpe::size<satisfactory_deps>::value << "): ";
+      detail::print_deps(satisfactory_deps{}); std::cout << "\n";
+
+      std::cout << "unsatisfactory dependencies (" << kmpe::size<unsatisfactory_deps>::value << "): ";
+      detail::print_deps(unsatisfactory_deps{}); std::cout << "\n";
     });
   }
 }
@@ -320,10 +505,22 @@ using namespace jln::fmtexpr::ext::print::types;
 
 int main(/*int argc, char **argv*/)
 {
-  JLN_FMTEXPR_VAR(xy, int_);
-  auto zx = named<class zx>(int_);
+  JLN_FMTEXPR_VAR(a);
+  JLN_FMTEXPR_TYPED_VAR(b, int_);
+  auto c = variable<class c>();
+  auto d = typed_name<class d>(int_);
+  auto e = expression{std::plus{}, a, c};
   int i = 3;
+  // auto expr = named<class r>(a + b) = eval(var<class r>);
 
-  auto packet = pack(xy(2), zx(i), int_(4));
+  auto packet = pack(
+    0, int_, int_(i), int_(0),
+    a, a(1),
+    a, a(1),
+    b, /*b(2),*/
+    c, c(i),
+    d, d(4),
+    e
+  );
   introspection(packet);
 }
